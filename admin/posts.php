05327de2bd7b94c9fca469465b70e587cb39877e
Alex <alexsandro.sousa12366@gmail.com>
2020-12-18 13:35:03 -0300

    \/\/\/\/

--- admin/posts.php
+++ admin/posts.php
@@ -19,84 +19,41 @@
                     <small>Author</small>
                 </h1>
 
-                <table class="table table-bordered table-hover">
-                    <thead>
-                        <tr>
-                            <th>Id</th>
-                            <th>Author</th>
-                            <th>Title</th>
-                            <th>Category</th>
-                            <th>Status</th>
-                            <th>Image</th>
-                            <th>Tags</th>
-                            <th>Comments</th>
-                            <th>Date</th>
-                        </tr>
-                    </thead>
+                <?php 
+                if(isset($_GET['source'])){
 
-                    <tbody>
+                    $source = $_GET['source'];
 
-                        <?php
+                } else {
+                    $source = '';
+                }
 
-                        $query = "SELECT * FROM posts";
-                        $select_posts = mysqli_query($connection,$query);
+                switch($source){
 
-                        while($row = mysqli_fetch_assoc($select_posts)){
-                            $post_id = $row['post_id'];
-                            $post_author = $row['post_author'];
-                            $post_title = $row['post_title'];
-                            $post_category_id = $row['post_category_id'];
-                            $post_status = $row['post_status'];
-                            $post_image = $row['post_image'];
-                            $post_tags = $row['post_tags'];
-                            $post_date = $row['post_date'];
+                    case 'add_post';
+                        include "includes/add_post.php";
+                    break;
 
-                            echo "<tr>";
-                            echo "<td>$post_id</td>";
-                            echo "<td>$post_author</td>";
-                            echo "<td>$post_title</td>";
-                            echo "<td>$post_category_id</td>";
-                            echo "<td>$post_status</td>";
-                            echo "<td>$post_image</td>";
-                            echo "<td>$post_tags</td>";
-                            echo "<td>$post_date</td>";
-                            echo "</tr>";
+                    case '100';
+                        echo "NICE 100";
+                    break;
 
+                    case '200';
+                        echo "NICE 200";
+                    break;
 
+                    default:
 
+                    include "includes/view_all_posts.php";
 
-
-
-
-
-
-                        }
-
-
-
-
-
-
-                        ?>
-
-
-
-
-                  
-                            <td>10</td>
-                            <td>Alex</td>
-                            <td>Bootstrap Framework</td>
-                            <td>Bootstrap</td>
-                            <td>Status</td>
-                            <td>Image</td>
-                            <td>Tags</td>
-                            <td>Comments</td>
-                            <td>Date</td>
-                        
-                    </tbody>
-
-                </table>
-
+                    break;
+                    
+                }
+                
+                
+                
+                
+                ?>
 
 
 
