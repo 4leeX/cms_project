eda0429e2979df038b32c10887089783167bde2d
Alex <alexsandro.sousa12366@gmail.com>
2021-01-01 10:31:29 -0300

    update-2.0

--- admin/index.php
+++ admin/index.php
@@ -16,7 +16,7 @@
 
                         <h1 class="page-header">
                            Welcome to admin
-                            <small>Author</small>
+                            <small><?= $_SESSION['username']; ?></small>
                         </h1>
                             
                         </ol>
---
+++ admin/profile.php
@@ -0,0 +1,4 @@
+<?php
+echo "chegamos até aqui";
+
+?>
